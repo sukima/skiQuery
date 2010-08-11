9defa926d44afa3bc6859a54a3dc58a85ea7792a
Devin Weaver <suki@tritarget.org>
2010-08-11 08:12:11 -0400

    Initial commit

---
+++ skiQuery.js
@@ -0,0 +1,18 @@
+/**
+ * This is ski %s, designed by Mark Stevans, ported by Eric S. Raymond.
+ * You are hurtling down a ski slope in reverse, trying to evade the Yeti.
+ * Available commands are:
+ *
+ *  l = turn left            r = turn right
+ *  j = jump                 h = hop
+ *  t = teleport             Enter = keep skiing
+ *  i = launch ICBM          d = summon Fire Demon
+ *
+ *  ! = interpret line as shell command and execute.
+ *  ? = print this help message.
+ */
+var SKI = { };
+SKI.version = '1.0';
+SKI.portVersion = '6.4';
+
+
